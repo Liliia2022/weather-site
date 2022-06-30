008acf363302c0b871c563030b115c599a49005b
Liliia2022 <zumbaliliia21@gmail.com>
2022-07-01 00:14:22 +0200

    corrected size of img and containers of forecast

--- script/index.js
+++ script/index.js
@@ -49,7 +49,7 @@ function displayForecast(response) {
         forecastDay.weather[0].icon
       }@2x.png"
           alt=""
-          width="42"
+          width="60"
         />
                 </div>
                 <div>
